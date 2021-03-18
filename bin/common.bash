34e8c6c7ae704f08b61a6e4a28af1b4acaa0b23f
Fredrik Liv <fredrik.liv@elastisys.com>
2021-03-18 10:19:08 +0100

    bin: Replace ~ with $HOME so absolute path expansion works

--- bin/common.bash
+++ bin/common.bash
@@ -7,9 +7,17 @@
 : "${CK8S_CONFIG_PATH:?Missing CK8S_CONFIG_PATH}"
 : "${prefix:?Missing prefix}"
 
+# Check for this mistake https://github.com/koalaman/shellcheck/wiki/SC2088
+# shellcheck disable=SC2088
+if [[ "${CK8S_CONFIG_PATH:0:2}" == "~/" ]]; then
+    echo "Warning: CK8S_CONFIG_PATH starts with unexpanded ~/" 1>&2
+    echo "This will create a new folder in cwd called ~ instead of referencing ${HOME}" 1>&2
+    echo "please use \${HOME} instead if that's what you want" 1>&2
+fi
+
 # Create CK8S_CONFIG_PATH if it does not exist and make it absolute
-mkdir -p "${CK8S_CONFIG_PATH}"
 CK8S_CONFIG_PATH=$(readlink -f "${CK8S_CONFIG_PATH}")
+mkdir -p "${CK8S_CONFIG_PATH}"
 export CK8S_CONFIG_PATH
 
 here="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
