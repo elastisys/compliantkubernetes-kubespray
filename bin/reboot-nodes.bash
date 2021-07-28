a1f159930e91016d9ae8b929fbd861e6dc7ede21
viktor_forsberg <viktor@elastisys.com>
2021-07-28 09:47:40 +0200

    bin: add command to reboot nodes

---
+++ bin/reboot-nodes.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script will run an ansible playbook.
+# It's not to be executed on its own but rather via `ck8s-kubespray reboot-nodes <prefix>`.
+# Add the variable "manual_prompt = true" for a manual prompt.
+
+set -eu -o pipefail
+
+here="$(dirname "$(readlink -f "$0")")"
+# shellcheck source=bin/common.bash
+source "${here}/common.bash"
+
+log_info "Running playbook reboot_nodes"
+pushd "${here}/../playbooks"
+
+ansible-playbook -i "${config[inventory_file]}" reboot_nodes.yml -b "$@"
+
+popd
+
+log_info "Playbook ran successfully!"
