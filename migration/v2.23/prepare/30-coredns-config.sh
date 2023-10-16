9c96e999d04f7c0db4c4a0b429de1890fc45ead7
Pavan-Gunda <pavan.gunda@elastisys.com>
2023-10-16 13:55:25 +0200

    coredns migration fix

--- migration/v2.23/prepare/30-coredns-config.sh
+++ migration/v2.23/prepare/30-coredns-config.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-HERE="$(dirname "$(readlink -f "${0}")")"
-ROOT="$(readlink -f "${HERE}/../../../")"
+set -e
 
-# shellcheck source=scripts/migration/lib.sh
-source "${ROOT}/scripts/migration/lib.sh"
+additional_error_config='consolidate 5m ".* i/o timeout$" warning'
 
-printf -v coredns_error "consolidate 5m '.* i/o timeout$' warning\n"
+tempfile=$(mktemp)
+trap 'rm -f "$tempfile"' EXIT
 
-yq_add sc "k8s_cluster/ck8s-k8s-cluster" .coredns_additional_error_config "\"$coredns_error\""
-yq_add wc "k8s_cluster/ck8s-k8s-cluster" .coredns_additional_error_config "\"$coredns_error\""
+printf "coredns_additional_error_config: |\n  %s\n" "$additional_error_config" > "$tempfile"
+
+yq4 eval-all 'select(fileIndex == 0) * select(fileIndex == 1)' "$CK8S_CONFIG_PATH"/wc-config/group_vars/k8s_cluster/ck8s-k8s-cluster.yaml "$tempfile" -i
+yq4 eval-all 'select(fileIndex == 0) * select(fileIndex == 1)' "$CK8S_CONFIG_PATH"/sc-config/group_vars/k8s_cluster/ck8s-k8s-cluster.yaml "$tempfile" -i
+
+rm -f "$tempfile"
