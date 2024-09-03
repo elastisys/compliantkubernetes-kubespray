073508450eb53b236c383143f9af95e80e6e3758
Axel Jarmar <axel.jarmar@elastisys.com>
2024-09-03 14:46:57 +0200

    script: only run lb legacy network migration script on upcloud environments

--- migration/v2.25/prepare/20-add-lb-legacy-network.sh
+++ migration/v2.25/prepare/20-add-lb-legacy-network.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
 
+HERE="$(dirname "$(readlink -f "${0}")")"
+ROOT="$(readlink -f "${HERE}/../../../")"
+
+# shellcheck source=scripts/migration/lib.sh
+source "${ROOT}/scripts/migration/lib.sh"
+
 if [[ "${CK8S_CLUSTER}" =~ ^(sc|both)$ ]]; then
-  tfvars_file="${CK8S_CONFIG_PATH}/sc-config/cluster.tfvars"
-  if ! grep -P "^loadbalancer_legacy_network" "${tfvars_file}" >/dev/null; then
-    echo "loadbalancer_legacy_network = true" >> "${tfvars_file}"
+  if [[ -f "${CK8S_CONFIG_PATH}/sc-config/group_vars/k8s_cluster/ck8s-k8s-cluster-upcloud.yaml" ]]; then
+    tfvars_file="${CK8S_CONFIG_PATH}/sc-config/cluster.tfvars"
+    if ! grep -P "^loadbalancer_legacy_network" "${tfvars_file}" >/dev/null; then
+      echo "loadbalancer_legacy_network = true" >> "${tfvars_file}"
+    fi
+  else
+    log_info "Not an UpCloud environment, skipping sc"
   fi
 fi
 if [[ "${CK8S_CLUSTER}" =~ ^(wc|both)$ ]]; then
-  tfvars_file="${CK8S_CONFIG_PATH}/wc-config/cluster.tfvars"
-  if ! grep -P "^loadbalancer_legacy_network" "${tfvars_file}" >/dev/null; then
-    echo "loadbalancer_legacy_network = true" >> "${tfvars_file}"
+  if [[ -f "${CK8S_CONFIG_PATH}/wc-config/group_vars/k8s_cluster/ck8s-k8s-cluster-upcloud.yaml" ]]; then
+    tfvars_file="${CK8S_CONFIG_PATH}/wc-config/cluster.tfvars"
+    if ! grep -P "^loadbalancer_legacy_network" "${tfvars_file}" >/dev/null; then
+      echo "loadbalancer_legacy_network = true" >> "${tfvars_file}"
+    fi
+  else
+    log_info "Not an UpCloud environment, skipping wc"
   fi
 fi
