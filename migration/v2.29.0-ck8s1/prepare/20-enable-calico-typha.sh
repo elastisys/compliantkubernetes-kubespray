4d3fe6a290545f267ecfbe76382e83e888cb70de
Mohamed Shafiq <mohamed.shafiq@elastisys.com>
2025-11-13 10:26:22 +0100

    fmt fix

--- migration/v2.29.0-ck8s1/prepare/20-enable-calico-typha.sh
+++ migration/v2.29.0-ck8s1/prepare/20-enable-calico-typha.sh
@@ -13,6 +13,6 @@ fi
 
 for cluster in "${CLUSTERS[@]}"; do
   #Enabling Typha and Typha metrics
-  yq_add ${cluster} k8s_cluster/ck8s-k8s-cluster .typha_enabled "load(\"${ROOT}/config/common/group_vars/k8s_cluster/ck8s-k8s-cluster.yaml\").typha_enabled"
-  yq_add ${cluster} k8s_cluster/ck8s-k8s-cluster .typha_prometheusmetricsenabled "load(\"${ROOT}/config/common/group_vars/k8s_cluster/ck8s-k8s-cluster.yaml\").typha_prometheusmetricsenabled"
+  yq_add "${cluster}" k8s_cluster/ck8s-k8s-cluster .typha_enabled "load(\"${ROOT}/config/common/group_vars/k8s_cluster/ck8s-k8s-cluster.yaml\").typha_enabled"
+  yq_add "${cluster}" k8s_cluster/ck8s-k8s-cluster .typha_prometheusmetricsenabled "load(\"${ROOT}/config/common/group_vars/k8s_cluster/ck8s-k8s-cluster.yaml\").typha_prometheusmetricsenabled"
 done
