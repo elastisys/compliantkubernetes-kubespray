a1946e1d1ef684cb42ba88eb25d7ba76d8def9ca
Mohamed Shafiq <mohamed.shafiq@elastisys.com>
2025-11-13 09:57:22 +0100

    fmt fix

--- migration/v2.29.0-ck8s1/prepare/20-enable-calico-typha.sh
+++ migration/v2.29.0-ck8s1/prepare/20-enable-calico-typha.sh
@@ -11,7 +11,7 @@ if [[ "${CK8S_CLUSTER}" != both ]]; then
   CLUSTERS=("${CK8S_CLUSTER}")
 fi
 
-for cluster in ${CLUSTERS[@]}; do
+for cluster in "${CLUSTERS[@]}"; do
   #Enabling Typha and Typha metrics
   yq_add ${cluster} k8s_cluster/ck8s-k8s-cluster .typha_enabled "load(\"${ROOT}/config/common/group_vars/k8s_cluster/ck8s-k8s-cluster.yaml\").typha_enabled"
   yq_add ${cluster} k8s_cluster/ck8s-k8s-cluster .typha_prometheusmetricsenabled "load(\"${ROOT}/config/common/group_vars/k8s_cluster/ck8s-k8s-cluster.yaml\").typha_prometheusmetricsenabled"
