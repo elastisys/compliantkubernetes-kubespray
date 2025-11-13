49ff38a1def3eaa291fc1bdcb3afa7c299158cb9
Mohamed Shafiq <mohamed.shafiq@elastisys.com>
2025-11-13 09:39:29 +0100

    adjusted to use clusterwise

--- migration/v2.29.0-ck8s1/prepare/20-enable-calico-typha.sh
+++ migration/v2.29.0-ck8s1/prepare/20-enable-calico-typha.sh
@@ -6,7 +6,12 @@ ROOT="$(readlink -f "${HERE}/../../../")"
 # shellcheck source=scripts/migration/lib.sh
 source "${ROOT}/scripts/migration/lib.sh"
 
-for cluster in "sc" "wc"; do
+CLUSTERS=("sc" "wc")
+if [[ "${CK8S_CLUSTER}" != both ]]; then
+  CLUSTERS=("${CK8S_CLUSTER}")
+fi
+
+for cluster in ${CLUSTERS[@]}; do
   #Enabling Typha and Typha metrics
   yq_add ${cluster} k8s_cluster/ck8s-k8s-cluster .typha_enabled "load(\"${ROOT}/config/common/group_vars/k8s_cluster/ck8s-k8s-cluster.yaml\").typha_enabled"
   yq_add ${cluster} k8s_cluster/ck8s-k8s-cluster .typha_prometheusmetricsenabled "load(\"${ROOT}/config/common/group_vars/k8s_cluster/ck8s-k8s-cluster.yaml\").typha_prometheusmetricsenabled"
