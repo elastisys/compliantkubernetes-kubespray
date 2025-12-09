ffd5f48d33d87c2e3871890725d99823dd446dca
Axel Jarmar <axel.jarmar@elastisys.com>
2025-12-09 16:18:52 +0100

    fix: pre-commit errors

--- migration/v2.29/prepare/30-cilium-extra-cni-flags.sh
+++ migration/v2.29/prepare/30-cilium-extra-cni-flags.sh
@@ -11,7 +11,6 @@ if [[ "${CK8S_CLUSTER}" != both ]]; then
   CLUSTERS=("${CK8S_CLUSTER}")
 fi
 
-
 for cluster in "${CLUSTERS[@]}"; do
   #Add extra cni flags
   yq_add "${cluster}" k8s_cluster/ck8s-cilium .ck8s_cilium "load(\"${ROOT}/config/common/group_vars/k8s_cluster/ck8s-cilium.yaml\").ck8s_cilium"
