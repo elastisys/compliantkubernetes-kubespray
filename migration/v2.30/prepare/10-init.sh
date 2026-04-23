b9233fecdad40835a0b1c3ff07bcab5affbffd74
Simon Kollberg <simonkollberg@gmail.com>
2026-04-23 11:30:03 +0200

    migration: Remove pinning from v2.30

--- migration/v2.30/prepare/10-init.sh
+++ migration/v2.30/prepare/10-init.sh
@@ -7,14 +7,8 @@ ROOT="$(readlink -f "${HERE}/../../../")"
 source "${ROOT}/scripts/migration/lib.sh"
 
 if [[ "${CK8S_CLUSTER}" =~ ^(sc|both)$ ]]; then
-  cp "${ROOT}/config/common/group_vars/all/ck8s-image-tags.yaml" "${CK8S_CONFIG_PATH}/sc-config/group_vars/all/"
-  cp "${ROOT}/config/common/group_vars/all/ck8s-kubeadm-patches.yaml" "${CK8S_CONFIG_PATH}/sc-config/group_vars/all/"
-  cp "${ROOT}/config/ck8s-kube-proxy-image" "${CK8S_CONFIG_PATH}/sc-config/"
   yq_add sc all/ck8s-kubespray-general .ck8sKubesprayVersion "\"$(git_version)\""
 fi
 if [[ "${CK8S_CLUSTER}" =~ ^(wc|both)$ ]]; then
-  cp "${ROOT}/config/common/group_vars/all/ck8s-image-tags.yaml" "${CK8S_CONFIG_PATH}/wc-config/group_vars/all/"
-  cp "${ROOT}/config/common/group_vars/all/ck8s-kubeadm-patches.yaml" "${CK8S_CONFIG_PATH}/wc-config/group_vars/all/"
-  cp "${ROOT}/config/ck8s-kube-proxy-image" "${CK8S_CONFIG_PATH}/wc-config/"
   yq_add wc all/ck8s-kubespray-general .ck8sKubesprayVersion "\"$(git_version)\""
 fi
